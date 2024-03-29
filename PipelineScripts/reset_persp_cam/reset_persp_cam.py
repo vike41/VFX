f25ccc25a6c35d3f2554b1cdb5c642a46d6b66f3
vike41 <vitalikkens@gmail.com>
2024-03-29 21:02:37 +0100

    Fix 4 Reset Camera

--- PipelineScripts/reset_persp_cam/reset_persp_cam.py
+++ PipelineScripts/reset_persp_cam/reset_persp_cam.py
@@ -1,12 +1,21 @@
 import pymel.core as pm
-def reset_persp_cam():
-    if pm.objExists("|persp"):
-        perspective_camera = pm.PyNode("|persp")
-        pm.camera(perspective_camera, e=True, startupCamera = False)
-        pm.delete(perspective_camera)
+class ResetPerspectiveCamera():
+    def __init__(self, perp_camera):
+        self.perp_camera = perp_camera
 
-    new_perspective_camera = pm.camera()
-    new_perspective_camera[0].rename("|persp")
-    new_perspective_camera[0].hide()
-    pm.camera(new_perspective_camera[0], e=True, startupCamera = True)
-    pm.reorder(new_perspective_camera[0], front=True)
+    def reset_persp_cam(self):
+        if pm.objExists("|persp"):
+            perspective_camera = pm.PyNode("|persp")
+            pm.camera(perspective_camera, e=True, startupCamera = False)
+            pm.delete(perspective_camera)
+    def create_new_perp_camera(self):
+        new_perspective_camera = pm.camera()
+        new_perspective_camera[0].rename("|persp")
+        new_perspective_camera[0].hide()
+        pm.camera(new_perspective_camera[0], e=True, startupCamera = True)
+        pm.reorder(new_perspective_camera[0], front=True)
+
+if __name__ == "__main__":
+    RPC = ResetPerspectiveCamera('camera')
+    RPC.reset_persp_cam()
+    RPC.create_new_perp_camera()
